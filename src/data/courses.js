56ef40fe4a7fb5125cb5958a5956a340c1e23b27
Ekiserrepé <ekiserrepe@gmail.com>
2026-02-27 09:18:54 +0100

    Rename files

--- src/data/courses.js
+++ src/data/courses.js
@@ -9,16 +9,16 @@
  */
 
 import m00 from './modules/m00-setup.js'
-import m01 from './modules/m01-blockchain-no-evm.js'
-import m02 from './modules/m02-consenso.js'
-import m03 from './modules/m03-primera-wallet.js'
-import m04 from './modules/m04-consulta-datos.js'
-import m05 from './modules/m05-anatomia-transacciones.js'
-import m06 from './modules/m06-pagos.js'
+import m01 from './modules/m01-blockchain-non-evm.js'
+import m02 from './modules/m02-consensus.js'
+import m03 from './modules/m03-first-wallet.js'
+import m04 from './modules/m04-querying-data.js'
+import m05 from './modules/m05-transaction-anatomy.js'
+import m06 from './modules/m06-payments.js'
 import m07 from './modules/m07-tokens.js'
 import m08 from './modules/m08-nfts.js'
 import m09 from './modules/m09-smart-contracts.js'
-import m10 from './modules/m10-escrows-cheques.js'
+import m10 from './modules/m10-escrows-checks.js'
 import m11 from './modules/m11-xaman-sdk.js'
 
 export const COURSE_DATA = [
